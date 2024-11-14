68efd7fb7e20854caa53288a6ae533b0b26cd709
Aamna Imdadullah <AamnaImdadullah@gmail.com>
2024-11-14 23:46:57 +0500

    first commit

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Header(){
+    return(
+        <div>
+            <div className="h-134px background: rgba(162, 152, 117, 1) width-1920px ;">
+            
+            </div>
+        </div>
+    );
+};
+export default Header;
